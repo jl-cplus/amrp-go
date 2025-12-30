a02f65cc0f22458b6c2f5208dd76dab7640a76e3
jl-cplus <218733449+jl-cplus@users.noreply.github.com>
2025-12-30 09:36:50 +0000

    feat: add controller logic and UI event handling

--- src/controller/main_controller.go
+++ src/controller/main_controller.go
@@ -3,3 +3,109 @@
 // license that can be found in the LICENSE file.
 
 package controller
+
+import (
+	"bufio"
+	"errors"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// progress listener for ui to receive updates
+type ProgressListener func(num int)
+
+func ProcessCSV(inputPath, outputPath string, progressListener ProgressListener) error {
+	// open input file
+	inFile, err := os.Open(inputPath)
+	if err != nil {
+		return err
+	}
+	defer inFile.Close()
+
+	// create output file
+	outFile, err := os.Create(outputPath)
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+
+	// buffer reader
+	reader := bufio.NewReader(inFile)
+	writer := bufio.NewWriter(outFile)
+	defer writer.Flush()
+
+	lineNum := 0
+	for {
+		line, err := reader.ReadString('\n')
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		// patch a record per line
+		if err, line = processInterceptor(line); err != nil {
+			return err
+		}
+
+		// write to output
+		if _, err := writer.WriteString(line); err != nil {
+			return err
+		}
+
+		// update progress
+		lineNum++
+		progressListener(lineNum)
+	}
+	return nil
+}
+
+// patching logic
+func processInterceptor(line string) (error, string) {
+	const size = 653
+	schema := []int{5, 28, 10, 10, 6, 2, 30, 140, 40, 140, 17, 17, 17, 6, 10, 12, 25, 25, 25, 25, 25, 17}
+
+	// patch header
+	if len(line) < size {
+		// validate schema
+		if len(strings.Split(line, ",")) != len(schema) {
+			return errors.New("Invalid CSV schema detected"), ""
+		}
+		node := strings.Split(strings.TrimSuffix(line, "\n"), ",")
+		for i := range node {
+			node[i] = strconv.Quote(node[i])
+		}
+		return nil, strings.Join(node, ",") + "\n"
+	}
+
+	// patch data
+	str := []string{}
+	begin := 0
+	for i, length := range schema {
+		end := begin + length
+		substr := line[begin:end]
+
+		// patch number format
+		if i == 10 || i == 11 {
+			substr = formatNumberString(substr)
+		}
+
+		str = append(str, "\""+substr+"\"")
+		begin = end + 1
+	}
+
+	return nil, strings.Join(str, ",") + "\n"
+}
+
+func formatNumberString(str string) string {
+	if strings.HasSuffix(str, "-") {
+		str = str[:len(str)-1]
+		num := strings.TrimSpace(str)
+		space := str[:len(str)-len(num)]
+		str = space[:len(space)-1] + "-" + num + " "
+	}
+	return str
+}
--- src/view/main_window.go
+++ src/view/main_window.go
@@ -5,7 +5,12 @@
 package view
 
 import (
+	"amrp-go/controller"
+	"amrp-go/util"
+	"amrp-go/view/extension"
+	"fmt"
 	"image/color"
+	"strings"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -30,21 +35,100 @@ func MainWindow() {
 }
 
 func mainContent(win fyne.Window) *fyne.Container {
-	// top: open file widget
+	var fpath string = ""
+	var proclog []string = []string{}
+	var await bool = false
+	// reset function
+	reset := func(w *OpenFileDialog) {
+		fpath = ""
+		await = false
+		// update on ui thread
+		fyne.Do(func() {
+			w.SetAwait(false)
+			w.Label.SetText(util.NO_FILE_SELECTED)
+		})
+	}
+
+	// top: openfile widget
 	openFile := &OpenFileDialog{}
 	openFile.SetVerbose(true)
 	openFile.OpenFileWidget(win, func(path string, err error) {
 		if err != nil {
 			dialog.ShowError(err, win)
 		}
+		// get file path
+		if path != "" {
+			fpath = path
+		}
 	})
 
 	// center: output textarea
 	textarea := widget.NewMultiLineEntry()
 	textarea.TextStyle = fyne.TextStyle{Monospace: true}
+	// widget's disable function causes its text to be unreadable on light themes.
+	// Workaround: prevent user data modification by listening to the onchange event
+	// and overriding the user input.
+	textarea.OnChanged = func(s string) {
+		textarea.SetText(strings.Join(proclog, ""))
+	}
 
 	// bottom: action button
-	actionBtn := widget.NewButtonWithIcon("Patch", theme.MailSendIcon(), func() {})
+	actionBtn := widget.NewButtonWithIcon("Patch", theme.MailSendIcon(), func() {
+		if await {
+			dialog.ShowInformation("", util.AwaitMessageRandom(), win)
+			return
+		}
+
+		if fpath == "" {
+			extension.ShowWarning("", util.NO_FILE_SELECTED, win)
+			return
+		}
+
+		// generate output filename
+		opath := util.RenameAppendSuffix(fpath, "new")
+		// update proclog
+		proclog = append(proclog, fmt.Sprintf("%-9s%-8s%-8s%s\n", util.HHMMSS(), "[INFO ]", "Source:", fpath))
+		proclog = append(proclog, fmt.Sprintf("%-9s%-8s%-8s%s\n", util.HHMMSS(), "[INFO ]", "Output:", opath))
+		// update ui
+		textarea.SetText(strings.Join(proclog, ""))
+
+		// prevent new processes from starting until the current one finishes
+		await = true
+		openFile.SetAwait(await)
+
+		go func() {
+			// receive progress updates
+			progressListener := func(num int) {
+				last := proclog[len(proclog)-1]
+				if strings.Contains(last, "Patching") {
+					proclog = proclog[:len(proclog)-1]
+				}
+				proclog = append(proclog, fmt.Sprintf("%-9s%-8sPatching total record(s) %d...\n", util.HHMMSS(), "[INFO ]", num))
+				// update on ui thread
+				fyne.Do(func() {
+					textarea.SetText(strings.Join(proclog, ""))
+				})
+			}
+			// start csv path process
+			err := controller.ProcessCSV(fpath, opath, progressListener)
+			if err != nil {
+				dialog.ShowError(err, win)
+				// update on ui thread
+				fyne.Do(func() {
+					proclog = append(proclog, fmt.Sprintf("%-9s%-8s%s\n", util.HHMMSS(), "[ERROR]", err.Error()))
+					textarea.SetText(strings.Join(proclog, ""))
+				})
+			} else {
+				// update on ui thread
+				fyne.Do(func() {
+					proclog = append(proclog, fmt.Sprintf("%-9s%-8s%s\n", util.HHMMSS(), "[INFO ]", "Completed successfully..."))
+					textarea.SetText(strings.Join(proclog, ""))
+				})
+			}
+			// reset
+			reset(openFile)
+		}()
+	})
 	actionBtn.Importance = widget.HighImportance
 
 	// border layout
